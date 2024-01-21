f34555ceec253828d750aec412f178fc8bd7857a
gros-pataplouf <sceres@pm.me>
2024-01-21 15:58:29 +0100

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -55,7 +55,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, date2, baseCost) {
-    let reduction = calculateReduction(date, date2);
+    let reduction = calculateReduction(date2);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
@@ -71,7 +71,7 @@ function createApp(database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date, date2) {
+  function calculateReduction(date2) {
     let reduction = 0;
     if (date2 && isMonday(date2) && !isHoliday(date2)) {
       reduction = 35;
